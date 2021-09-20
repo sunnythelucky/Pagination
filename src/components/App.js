c89bf3fc5b7411f905055d84f6ddd1c0e9b9e645
sunnythelucky <sunnyparkk@gmail.com>
2021-09-20 18:35:54 -0400

    add Pagination

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,101 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import unsplash from "../unsplash";
 import ImageList from "./ImageList";
+import { AppBar, Container, Toolbar, Typography, Pagination } from "@mui/material";
+import { PhotoCamera } from "@mui/icons-material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-class App extends React.Component {
-	state = { images: [] };
+const theme = createTheme();
 
-	onSearchSubmit = async (term) => {
+const App = () => {
+	const [images, setImages] = useState([]);
+	const [pages, setPages] = useState(0);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [term, setTerm] = useState("");
+	const [termChange, setTermChange] = useState(false);
+
+	const history = useHistory();
+
+	// const setHistory = (term) => {
+	// 	const params = new URLSearchParams();
+	// 	if (term) {
+	// 		params.append("search", term);
+	// 	}
+
+	// 	history.push({
+	// 		search: params.toString(),
+	// 	});
+	// };
+
+	useEffect(() => {
+		setCurrentPage(1);
+		onSearchSubmit();
+	}, [term]);
+
+	useEffect(() => {
+		onSearchSubmit();
+	}, [currentPage]);
+
+	const onSearchSubmit = async () => {
+		// setHistory(term);
+
 		const response = await unsplash.get("/search/photos", {
-			params: { query: term },
-			per_page: 20,
+			params: { query: term, page: currentPage },
 		});
-		this.setState({ images: response.data.results });
+
+		setImages(response.data.results);
+		setPages(response.data.total_pages);
 	};
-	render() {
-		return (
-			<div className="ui container" style={{ marginTop: "10px" }}>
-				<SearchBar handleSubmit={this.onSearchSubmit} />
-				<ImageList images={this.state.images} />
-			</div>
-		);
-	}
-}
+
+	const handleChangePage = (_, page) => {
+		setCurrentPage(page);
+	};
+
+	return (
+		<ThemeProvider theme={theme}>
+			<AppBar position="relative">
+				<Toolbar>
+					<PhotoCamera />
+					<Typography variant="h6">Photo Search</Typography>
+				</Toolbar>
+			</AppBar>
+			<main style={{ marginTop: "40px" }}>
+				<Container component="main" maxWidth="lg">
+					<SearchBar handleSubmit={setTerm} />
+				</Container>
+
+				<div style={{ backgroundColor: "palette.background.paper", padding: "spacing(8,0,6)" }}>
+					<Container maxWidth="sm">
+						<Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+							Photo Album
+						</Typography>
+						<Typography variant="h5" align="center" color="textSecondary" paragraph>
+							Velit commodo dolore ullamco nulla elit exercitation non velit mollit eiusmod in ea veniam commodo.
+							Exercitation deserunt do ipsum pariatur tempor aute sit occaecat ullamco aute magna deserunt aute. Ullamco
+							duis aliqua ex cupidatat minim dolore cillum laborum ad magna nostrud. Non anim nostrud adipisicing ut
+							pariatur. Nostrud sunt nisi officia quis et qui veniam ea nostrud. Qui aute do amet consectetur
+							adipisicing in occaecat esse mollit officia elit.
+						</Typography>
+					</Container>
+				</div>
+				<Container style={{ padding: "20px 0" }} maxWidth="md">
+					<ImageList images={images} />
+					<Pagination count={pages} page={currentPage} onChange={handleChangePage} color="secondary" />
+				</Container>
+			</main>
+
+			<footer style={{ backgroundColor: "palette.background.paper", padding: "50px 0" }}>
+				<Typography variant="h6" align="center" gutterBottom>
+					Footer
+				</Typography>
+				<Typography variant="subtitle1" align="center" color="textSecondary">
+					Something here to give the footer a purpose!
+				</Typography>
+			</footer>
+		</ThemeProvider>
+	);
+};
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,13 @@
-import React from "react";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme();
 
 class SearchBar extends React.Component {
 	state = { term: "" };
@@ -8,25 +17,50 @@ class SearchBar extends React.Component {
 
 		this.props.handleSubmit(this.state.term);
 	};
-
 	render() {
 		return (
-			<div className="ui segment">
-				<form onSubmit={this.onFormSubmit} className="ui form">
-					<div className="field">
-						<label>Image Search</label>
-						<input
-							type="text"
-							value={this.state.term}
-							onChange={(e) => {
-								this.setState({ term: e.target.value });
-							}}
-						/>
-					</div>
-				</form>
-			</div>
+			<>
+				<CssBaseline />
+				<Box
+					md={{
+						marginTop: 8,
+						display: "flex",
+						flexDirection: "row",
+						alignItems: "center",
+					}}
+					sm={{
+						marginTop: 8,
+						display: "flex",
+						flexDirection: "row",
+						alignItems: "center",
+					}}
+				>
+					<Box component="form" noValidate onSubmit={this.onFormSubmit}>
+						<Grid container spacing={2}>
+							<Grid item md={10}>
+								<TextField
+									fullWidth
+									name="searchBar"
+									label="searchBar"
+									type="searchBar"
+									id="searchBar"
+									value={this.state.term}
+									onChange={(e) => {
+										this.setState({ term: e.target.value });
+									}}
+								/>
+							</Grid>
+							<Grid item md={2}>
+								<Button type="submit" fullWidth variant="contained" md={{ mb: 2 }}>
+									Search
+								</Button>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+			</>
 		);
 	}
 }
 
-export default SearchBar; 
+export default SearchBar;
